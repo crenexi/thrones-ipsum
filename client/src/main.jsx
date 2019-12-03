d755c74aa449a77b3591e53539598a15553ba995
James Walrath <james@crenexi.com>
2019-12-02 21:59:47 -0800

    Commenting out some things to start boilerplate testing.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ThemeProvider } from '@material-ui/styles';
-import { buildIconLibrary } from 'config/font-awesome-config';
-import { baseTheme } from 'config/material-ui-config';
-import App from 'components/App/App';
-import './main.scss';
+// import { ThemeProvider } from '@material-ui/styles';
+// import { buildIconLibrary } from 'config/font-awesome-config';
+// import { baseTheme } from 'config/material-ui-config';
+// import App from 'components/App';
+// import './main.scss';
 
 // Build icon library
-buildIconLibrary();
+// buildIconLibrary();
 
 // Root markup
+// const Root = (
+//   <ThemeProvider theme={baseTheme()}>
+//     <h1>Hello World</h1>
+//     {/* <App /> */}
+//   </ThemeProvider>
+// );
+
 const Root = (
-  <ThemeProvider theme={baseTheme()}>
-    <App />
-  </ThemeProvider>
+  <h1>Hello World!</h1>
 );
 
 ReactDOM.render(Root, document.getElementById('root'));
--- client/src/routes/design/index.jsx
+++ client/src/routes/design/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Design from 'components/Design/Design';
+import Design from 'components/Design';
 
 const DesignRoute = () => (
   <Design />
